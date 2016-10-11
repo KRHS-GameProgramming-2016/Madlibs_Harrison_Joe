f82c03cb27eca823396f874ac2d6559887ade5e1
harrison lord <harrison.lord@kearsarge.org>
2016-10-11 11:35:29 -0400

    story 1 edit

--- story1.py
+++ story1.py
@@ -1,6 +1,6 @@
 from input import *
 
-#Written by Joseph Poltack and  
+#Written by Joseph Poltack and Harrison Lord
 def story():
     location1 = getWord("Enter a location: ")
     temperature1 = getNumber("Enter a Number: ")
