54fcb6fd35b7c0139f56dd020b96a8726dea1974
jpoltack <joseph.poltack@kearsarge.org>
2016-10-11 11:24:49 -0400

    more of story 1

--- story1.py
+++ story1.py
@@ -1,6 +1,6 @@
 from input import *
 
-#Written by Mr. Spooner
+#Written by Joseph Poltack and 
 def story():
     location1 = getWord("Enter a location: ")
     temperature1 = getNumber("Enter a Number: ")
