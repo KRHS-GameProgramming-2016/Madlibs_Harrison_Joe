1db64687f000199d4b6ce7cc5d7c7fb25e0a0a3b
jpoltack <joseph.poltack@kearsarge.org>
2016-10-12 11:36:09 -0400

    fix

--- story1.py
+++ story1.py
@@ -2,15 +2,17 @@ from input import *
 
 #Written by Joseph Poltack and Harrison Lord
 def story():
-    name1 = getWord("Enter a name")
-    name2 = getWord("Enter a name")
-    building = getWord("Enter a building")
-    food1 = getWord("Enter a food item")
+    name1 = getWord("Enter a name ")
+    name2 = getWord("Enter a name ")
+    building1 = getWord("Enter a building ")
+    food1 = getWord("Enter a food item ")
+    
+    
     text = ""
     text += "Two people named " + name1
-    text += "and" + name2
-    text += "lived in a." + building1
-    text += "They were both hungry so they went out to get some" + food1
+    text += " and " + name2
+    text += " lived in a. " + building1
+    text += " They were both hungry so they went out to get some " + food1
 
     
     
