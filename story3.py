469ed6c796d1f8d2694ccf51a546cd2df61d1ffb
harrison lord <harrison.lord@kearsarge.org>
2016-10-13 11:15:23 -0400

    hg

--- story3.py
+++ story3.py
@@ -7,17 +7,20 @@ def story():
     word1 = getWord("enter a word")
     place1 = getWord("enter a place")
     name1 = getWord("enter a food")
+    thing1 = getWord("enter a thing")
     text = ""
     text = "There is a person named" + name1
     text += "He is a" + word1
     text += "he came from" + place 1
     text += "he likes to eat" + food1
     text += "he hates" + thing1
-
-    
-    
-    
-    
+    text +=
+    text +=
+    text +=
+    text +=
+    text +=
+    text +=
+    text +=
       
     return text
 
