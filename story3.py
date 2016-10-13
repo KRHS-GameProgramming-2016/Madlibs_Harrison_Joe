49d718334fa37534d4bbf5130939de1329d57a4c
harrison lord <harrison.lord@kearsarge.org>
2016-10-13 10:42:48 -0400

    bgrh

--- story3.py
+++ story3.py
@@ -1,10 +1,25 @@
 from input import *
 
 #Written by Harrison Lord
+
 def story():
-day1
+    name1 = getWord("enter a name")
+    word1 = getWord("enter a word")
+    place1 = getWord("enter a place")
+    name1 = getWord("enter a food")
+    text = ""
+    text = "There is a person named" + name1
+    text += "He is a" + word1
+    text += "he came from" + place 1
+    text += "he likes to eat" + food1
+    text += "he hates" + thing1
 
-
+    
+    
+    
+    
+      
+    return text
 
 
 
