1d3b78a8a9c67a5b9fcb1c7478ee9c35c43ee73d
jpoltack <joseph.poltack@kearsarge.org>
2016-10-14 11:39:36 -0400

    5t4.

--- story2.py
+++ story2.py
@@ -6,15 +6,15 @@ def story():
     food1 = getWord("Enter a food item")
     shoebrand1 = getWord("Enter shoe brand name")
     
-    text = ""
-    text +="It was a" + day1
-    text +="morning and I was hungry."
-    text +="I dicited to have" + food1
-    text +="for beckfast."
-    text +="When I finished beakfast I then got drested and put on my " + shoebrand1
-    text +="shoes."
-    text +="After that I headed right out the door and started my car."
-
+    text =  ""
+    text += "It was a" + day1
+    text += "morning and I was hungry."
+    text += "I dicited to have" + food1
+    text + ="for beckfast."
+    text += "When I finished beakfast I then got drested and put on my " + shoebrand1
+    text += "shoes."
+    text += "After that I headed right out the door and started my car."
+    text += "
 
 
 
